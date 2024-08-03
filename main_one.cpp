114a4d3e97b2677862e2f5c8350f0da1371fb51e
Enrico Barbuio <148079534+enro284@users.noreply.github.com.>
2024-08-03 15:29:27 +0200

    Minor fix of variable names and declarations.

--- main_one.cpp
+++ main_one.cpp
@@ -1,25 +1,29 @@
-#include <iostream>
-
 #include "kinematics.hpp"
+#include <cassert>
+#include <iostream>
 
 int main()
 {
-  Pol pol{std::vector<double>{1.5, -0.2}};
   double l{4};
+  double r1{1.5};
+  double r2{0.7};
+
+  Pol pol{std::vector<double>{r1, (r2-r1)/l}};
   Barrier barrier_up{pol, l};
   Barrier barrier_down{-pol, l};
 
   while (true) {
     double y0{0};
-    double m{0};
+    double m0{0};
     std::cout << "enter y0: ";
     std::cin >> y0;
-    std::cout << "enter m: ";
-    std::cin >> m;
+    assert(y0 <= r1 && y0 >= -r1);
+    std::cout << "enter m0: ";
+    std::cin >> m0;
 
     Result res =
-        simulate_single_particle(barrier_up, barrier_down, Point{0, y0}, m);
+        simulate_single_particle(barrier_up, barrier_down, Point{0, y0}, m0);
 
-    std::cout << "result (x, y, theta): " << res << '\n';
+    std::cout << "result (x, y, theta[rad]): " << res << '\n';
   }
 }
