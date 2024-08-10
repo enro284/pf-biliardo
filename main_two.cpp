5f30db860845a2723d63869ca4b91fea760cc514
Alessandro Bruschi <alessandrobruschi@Mac-di-Alessandro.local>
2024-08-10 15:40:10 +0200

    Complete main_two.cpp

--- main_two.cpp
+++ main_two.cpp
@@ -1,4 +1,5 @@
 #include "kinematics.hpp"
+#include "statistics.hpp"
 #include <random>
 int main()
 {
@@ -21,9 +22,33 @@ int main()
   std::normal_distribution y_dist{u_y, sigma_y};
   std::normal_distribution theta_dist{u_theta, sigma_theta};
 
+  Sample statistics_y;
+  Sample statistics_theta;
+
   for (int i{0}; i != N; ++i) {
     double y0{y_dist(eng)};
+    while (std::abs(y0) > r1) {
+      y0 = y_dist(eng);
+    }
     double m0{std::tan(theta_dist(eng))};
-    simulate_single_particle(barrier_up, barrier_down, Point{0, y0}, m0);
+    Result res =
+        simulate_single_particle(barrier_up, barrier_down, Point{0, y0}, m0);
+    double yf     = res.get_y();
+    double thetaf = res.get_theta();
+    statistics_y.add(yf);
+    statistics_theta.add(thetaf);
   }
+
+  const auto result_y     = statistics_y.statistics();
+  const auto result_theta = statistics_theta.statistics();
+
+  std::cout << "The exit y values have a mean of " << result_y.mean
+            << " , a standard deviation of " << result_y.std_dev
+            << ", a skewnes coefficient of " << result_y.skewness
+            << " and a kurtosis of " << result_y.kurtosis << '\n';
+
+  std::cout << "The exit angle values have a mean of " << result_theta.mean
+            << " , a standard deviation of " << result_theta.std_dev
+            << ", a skewnes coefficient of " << result_theta.skewness
+            << " and a kurtosis of " << result_theta.kurtosis << '\n';
 }
