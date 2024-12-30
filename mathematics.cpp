5737c40ff8ac6c1e8cbe253a23d601c8291db576
Enrico Barbuio <148079534+enro284@users.noreply.github.com.>
2024-12-30 18:16:22 +0100

    Add support for second degree equations. TODO test

--- mathematics.cpp
+++ mathematics.cpp
@@ -36,7 +36,7 @@ Pol Pol::operator-()
   return res;
 }
 
-double eq_solve(Pol const& pol1, Pol const& pol2)
+std::vector<double> eq_solve(Pol const& pol1, Pol const& pol2)
 {
   std::vector<double> eq;
   std::vector<double> minus;
@@ -50,14 +50,72 @@ double eq_solve(Pol const& pol1, Pol const& pol2)
     eq     = pol1.coeff();
     minus  = pol2.coeff();
   }
+
+  minus.resize(eq_deg, 0.0);
+
   std::transform(minus.begin(), minus.end(), eq.begin(), eq.begin(),
                  [](double m, double e) { return e - m; });
+
+  std::vector<double> sol;
   switch (eq_deg) {
-  case 1:
-    return -eq[0] / eq[1];
-    break;
+  case 1: { // ax + b = 0
+    double a = eq[1];
+    double b = eq[0];
+    if (std::abs(a) < 1e-10) { // TODO: EPS
+      break;
+    }
+    sol.push_back({-b / a});
+  }
+  case 2: { // ax^2 + bx + c = 0
+    double a = eq[2];
+    double b = eq[1];
+    double c = eq[0];
+
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+      break;
+    } else if (std::abs(discriminant) < 1e-10) {
+      sol.push_back({-b / (2 * a)});
+    } else {
+      double sqrt_disc = std::sqrt(discriminant);
+      sol.push_back((-b - sqrt_disc) / (2 * a));
+      sol.push_back((-b + sqrt_disc) / (2 * a));
+    }
+  }
   default:
-    throw std::runtime_error("equation degree too high, I cannot solve it");
-    break;
-  } //used switch to make the implementation of higher degree polinomials easier
+    throw std::runtime_error(
+        "Equation degree too high, only 2nd degree is supported");
+  }
+  return sol;
+}
+
+bool Vec2::operator==(Vec2 const& rhs) const
+{
+  return (this->x_ == rhs.x_ && this->y_ == rhs.y_);
+}
+
+double dot(Vec2 const& rhs, Vec2 const& lhs)
+{
+  return rhs.x_ * lhs.x_ + rhs.y_ + rhs.y_;
+}
+
+double Vec2::norm() const
+{
+  return std::sqrt(x_ * x_ + y_ * y_);
+}
+
+Vec2 Vec2::ortho() const
+{
+  return {-y_, x_};
+}
+
+Vec2 operator+(Vec2 const& lhs, Vec2 const& rhs)
+{
+  return {lhs.x_ + rhs.x_, lhs.y_ + rhs.y_};
+}
+
+Vec2 Vec2::operator*(double rhs) const
+{
+  return {x_ * rhs, y_ * rhs};
 }
--- mathematics.hpp
+++ mathematics.hpp
@@ -5,12 +5,13 @@
 
 class Pol
 {
-  std::vector<double> coeff_; 
+  std::vector<double> coeff_;
 
  public:
   Pol(std::vector<double> coeff);
   double operator()(double x);
-  //double der(double x); //Operator to calculate derivative, needs to be implemented for higher degree barriers
+  // double der(double x); //Operator to calculate derivative, needs to be
+  // implemented for higher degree barriers
 
   int deg() const;
   std::vector<double> coeff() const;
@@ -18,6 +19,20 @@ class Pol
   Pol operator-();
 };
 
-double eq_solve(Pol const& pol1, Pol const& pol2);
+std::vector<double> eq_solve(Pol const& pol1, Pol const& pol2);
+
+struct Vec2
+{
+  double x_;
+  double y_;
+
+  bool operator==(Vec2 const& rhs) const;
+  Vec2 operator*(double rhs) const; // TODO: make free func
+  double norm() const;
+  Vec2 ortho() const;
+};
+
+Vec2 operator+(Vec2 const& lhs, Vec2 const& rhs);
+double dot(Vec2 const& rhs, Vec2 const& lhs);
 
 #endif
